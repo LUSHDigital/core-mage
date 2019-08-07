50775272d296395b1e8a2555a44942bd7f37b327
zeeraw <zee@vall.in>
2019-08-07 12:30:23 +0200

    feat: add mysql database

---
+++ targets/compose/mysql.go
@@ -0,0 +1,42 @@
+package compose
+
+var (
+	// MySQLService represents a docker compose mysql service.
+	MySQLService = Service{
+		Image: "mysql:5.7",
+		Logging: map[string]string{
+			"driver": "none",
+		},
+		Restart: "always",
+		Ports: []string{
+			"3306:3306",
+		},
+		Volumes: []string{
+			"${PWD}/data/mysql/dev:/var/lib/mysql",
+		},
+		Environment: map[string]string{
+			"MYSQL_DATABASE":      "service",
+			"MYSQL_USER":          "user",
+			"MYSQL_PASSWORD":      "passwd",
+			"MYSQL_ROOT_PASSWORD": "passwd",
+		},
+	}
+
+	// MySQLTestService represents a docker compose mysql service for the test environment.
+	MySQLTestService = Service{
+		Image: "mysql:5.7",
+		Logging: map[string]string{
+			"driver": "none",
+		},
+		Restart: "always",
+		Volumes: []string{
+			"${PWD}/data/mysql/test:/var/lib/mysql",
+		},
+		Environment: map[string]string{
+			"MYSQL_DATABASE":      "service",
+			"MYSQL_USER":          "user",
+			"MYSQL_PASSWORD":      "passwd",
+			"MYSQL_ROOT_PASSWORD": "passwd",
+		},
+	}
+)
