2859195f8d386ccc9803e03f4e30fa3e86461699
zeeraw <zee@vall.in>
2019-08-08 12:20:43 +0200

    fix: use the correct gopath

--- targets/compose/app.go
+++ targets/compose/app.go
@@ -9,7 +9,7 @@ var (
 		Restart: "no",
 		Volumes: []string{
 			"$PWD:/service:ro",
-			"$GOMODPATH:/usr/local/go/pkg/mod:ro",
+			"$GOMODPATH:/go/pkg/mod:ro",
 		},
 		WorkingDir: "/service",
 	}
@@ -21,7 +21,7 @@ var (
 		Restart: "no",
 		Volumes: []string{
 			"$PWD:/service:ro",
-			"$GOMODPATH:/usr/local/go/pkg/mod:ro",
+			"$GOMODPATH:/go/pkg/mod:ro",
 		},
 		WorkingDir: "/service",
 	}
